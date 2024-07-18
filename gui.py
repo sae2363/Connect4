2aafcb3ded8507b77d64197f98abd0efdd6fba23
BworldA <alfonsoarnal1@gmail.com>
2024-07-18 15:25:04 -0600

    tkinter test GUI file

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ m = tkinter.Tk()
 widgets are added here
 '''
 
-frm = ttk.Frame(m, padding=100)
+frm = ttk.Frame(m, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=m.destroy).grid(column=1, row=0)
