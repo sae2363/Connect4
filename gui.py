c1ad669fbcb1595e00731a68a079b22cc912c432
BworldA <alfonsoarnal1@gmail.com>
2024-07-18 15:20:44 -0600

    Added a tkinter test in the GUI file

--- gui.py
+++ gui.py
@@ -1,7 +1,17 @@
 import tkinter
+from tkinter import ttk
 
 m = tkinter.Tk()
+
 '''
 widgets are added here
 '''
+
+frm = ttk.Frame(m, padding=100)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=m.destroy).grid(column=1, row=0)
+
+# Test to make sure Tkinter works
+
 m.mainloop()
