f6164b33fa79b0f48a81f2e8873dea398691fd0d
sae <135391887+sae2363@users.noreply.github.com>
2024-07-22 15:39:17 -0400

    Update point.py 3:34 7-22

--- Agents/point.py
+++ Agents/point.py
@@ -7,5 +7,11 @@ class point:
         self.y=yValue
     def __str__(self):
         return "("+str(self.x)+", "+str(self.y)+")"
+    def __repr__(self):
+        return self.__str__()
     def checkVal(self,board,player):
         return board[self.y][self.x]==player 
+    def __eq__(self, other):
+        if isinstance(other, point):
+            return self.x == other.x and self.y == other.y
+        return NotImplemented
