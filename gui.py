da54178dabc561c0cdfb39b24a26179127da5d91
BworldA <alfonsoarnal1@gmail.com>
2024-07-23 09:56:39 -0600

    Split GUI into different functions

--- gui.py
+++ gui.py
@@ -3,33 +3,21 @@ from tkinter import *
 import numpy as np
 
 class GUI:
-    def __init__(self, root, state):
+    def __init__(self, root):
         self.root = root
         self.root.geometry("800x500")
         self.root.title("Connect 4")
 
-        # Below is the connect 4 board 
 
-        gridSizeX = len(state) # Check the length of the array to know how many squares the grid needs to be
-        gridSizeY = len(state[0])
-
-        self.board = tk.Frame(root, height=300, width=300, relief='solid', borderwidth=1) # creaste a Frame object called board
-
-        for j in range(gridSizeX): # all the columns
-            self.board.grid_columnconfigure(j,  weight =1, minsize=300/gridSizeX)
-            for i in range(gridSizeY): # specify how many rows and columns we will need
-                self.board.grid_rowconfigure(i,  weight =1, minsize=300/gridSizeY)
-                if int(state[j][i]) == 1:
-                    foregroundColor = 'blue'
-                elif int(state[j][i]) == 2:
-                    foregroundColor = 'red'
-                else: foregroundColor = 'black'
-                Label(self.board, text=int(state[j][i]), foreground=foregroundColor).grid(row=i, column=j)
-
-        self.board['relief'] = 'solid'
-        self.board.grid(row=1, column=1) # Draw the board
+    def createBoard(self, state):
+      
+        self.updateBoard(state)
 
         # Below is the code for the buttons
+
+        
+        gridSizeX = len(state) # Check the length of the array to know how many squares the grid needs to be
+        gridSizeY = len(state[0])
 
         self.buttonFrame = tk.Frame(root, height=20, width=300) # creaste a Frame object called board
 
@@ -53,6 +41,30 @@ class GUI:
         self.label1.grid(row=0, column=0)
 
 
+    def updateBoard(self, state):
+        
+        # Below is the connect 4 board 
+
+        gridSizeX = len(state) # Check the length of the array to know how many squares the grid needs to be
+        gridSizeY = len(state[0])
+
+        self.board = tk.Frame(root, height=300, width=300, relief='solid', borderwidth=1) # creaste a Frame object called board
+
+        for j in range(gridSizeX): # all the columns
+            self.board.grid_columnconfigure(j,  weight =1, minsize=300/gridSizeX)
+            for i in range(gridSizeY): # specify how many rows and columns we will need
+                self.board.grid_rowconfigure(i,  weight =1, minsize=300/gridSizeY)
+                if int(state[j][i]) == 1:
+                    foregroundColor = 'blue'
+                elif int(state[j][i]) == 2:
+                    foregroundColor = 'red'
+                else: foregroundColor = 'black'
+                Label(self.board, text=int(state[j][i]), foreground=foregroundColor).grid(row=i, column=j)
+
+        self.board['relief'] = 'solid'
+        self.board.grid(row=1, column=1) # Draw the board
+
+
     def on_button_click(self, column): # this function recieves the value of whatever move was selected (which button got pressed)
         column = column[1]
         self.label1 = tk.Label(root, text=(column, "Button Clicked!"))
@@ -65,5 +77,6 @@ testState[1][1] = '1'
 testState[1][2] = '2'
 testState[2][2] = '1'
 root = tk.Tk()
-app = GUI(root, state=testState)
+app = GUI(root)
+app.createBoard(state=testState)
 root.mainloop()
